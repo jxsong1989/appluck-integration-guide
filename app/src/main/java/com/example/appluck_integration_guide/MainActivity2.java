efadb67a13fa3c2d9a7e853d89067cb528c4cd9c
jason <jxsong1989@163.com>
2022-08-08 12:13:28 +0800

    支持market

--- app/src/main/java/com/example/appluck_integration_guide/MainActivity2.java
+++ app/src/main/java/com/example/appluck_integration_guide/MainActivity2.java
@@ -1,11 +1,17 @@
 package com.example.appluck_integration_guide;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.webkit.WebViewClientCompat;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -16,7 +22,19 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         webView = findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClientCompat());
+        webView.setWebViewClient(new WebViewClientCompat(){
+            @Override
+            public boolean shouldOverrideUrlLoading(@NonNull WebView view, @NonNull WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                if(StringUtils.startsWith(url,"market:")){
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
         webView.loadUrl(CommonUtils.slotUrlWithGaid);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true); // 允许javascript执行
