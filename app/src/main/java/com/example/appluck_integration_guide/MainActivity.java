8556a027b322661a012f9b4d4f57d7a297b91a90
jason <jxsong1989@163.com>
2023-01-28 18:53:48 +0800

    修改头处理

--- app/src/main/java/com/example/appluck_integration_guide/MainActivity.java
+++ app/src/main/java/com/example/appluck_integration_guide/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.appluck_integration_guide;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -66,16 +68,34 @@ public class MainActivity extends AppCompatActivity {
                 WebViewClient webViewClient = new WebViewClient() {
                     @Override
                     public boolean shouldOverrideUrlLoading(@NonNull WebView view, @NonNull WebResourceRequest request) {
-                        String url = request.getUrl().toString();
-                        Log.d("url", url);
-                        //支持google play
-                        if (StringUtils.startsWith(url, "market:")
-                                || StringUtils.startsWith(url, "https://play.google.com/store/")
-                                || StringUtils.startsWith(url, "http://play.google.com/store/")) {
-                            Intent intent = new Intent(Intent.ACTION_VIEW);
-                            intent.setData(Uri.parse(url));
-                            startActivity(intent);
-                            return true;
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            String url = request.getUrl().toString();
+                            Log.d("url", url);
+                            //支持google play
+                            try {
+                                //支持google play
+                                if (StringUtils.startsWith(url, "market:")
+                                        || StringUtils.startsWith(url, "https://play.google.com/store/")
+                                        || StringUtils.startsWith(url, "http://play.google.com/store/")) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                                    intent.setData(Uri.parse(url));
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                    return true;
+                                } else if (!StringUtils.startsWith(url, "http://")
+                                        && !StringUtils.startsWith(url, "https://")) {
+                                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                                    ActivityInfo activityInfo = intent.resolveActivityInfo(getPackageManager(), 0);
+                                    if (activityInfo.exported) {
+                                        intent.setData(Uri.parse(url));
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                    }
+                                    return true;
+                                }
+                            } catch (Throwable e) {
+                                return true;
+                            }
                         }
                         return super.shouldOverrideUrlLoading(view, request);
                     }
